55bac0e5361a30b729248ec76b20ca5934facfc1
Your Name <you@example.com>
2024-10-09 16:47:40 +0530

    Made social media section phone responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,3 @@
-// const listItems = document.querySelectorAll(".upper-nav li");
-
-// listItems.forEach((item) => {
-//   item.addEventListener("click", function () {
-//     listItems.forEach((li) => li.classList.remove("active"));
-
-//     this.classList.add("active");
-//   });
-// });
-
 // Section two navbar active button bounce animation
 document.addEventListener("DOMContentLoaded", function () {
   if (window.innerWidth < 560) {
